03ab7f5007bee530a5724f4671159fc1eccb4f1c
razvanr91 <razvanroman1991@gmail.com>
2021-07-27 11:36:05 +0100

    Create Blog Model

---
+++ Models/Blog.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBlogProject.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage ="The {0} must be between {2} and {1} characters long.",MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Created on ")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Updated on ")]
+        public DateTime? Updated { get; set; }
+
+        [Display(Name = "Blog Image")]
+        public byte[] ImageData { get; set; }
+
+        [Display(Name = "Image Type")]
+        public string ContentType { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+    }
+}

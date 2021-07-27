c3cc289669bfc25fe4e0a1bcd9f5db195ac1d8a7
razvanr91 <razvanroman1991@gmail.com>
2021-07-27 11:58:17 +0100

    Add Tag Model

---
+++ Models/Tag.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBlogProject.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 2)]
+        public string Text { get; set; }
+
+        public virtual Post Post { get; set; }
+
+        public virtual IdentityUser Author { get; set; }
+    }
+}

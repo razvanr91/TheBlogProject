0f7c4242703e28b9ce0a47f137383ce4bc354c23
razvanr91 <razvanroman1991@gmail.com>
2021-07-27 15:16:35 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -38,9 +38,13 @@ namespace TheBlogProject
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddIdentity<BlogUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddDefaultUI()
+                .AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddControllersWithViews();
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

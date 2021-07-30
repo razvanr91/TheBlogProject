c6a3989f65730264d2a5933ae5cbd67a02640c2e
razvanr91 <razvanroman1991@gmail.com>
2021-07-30 15:59:46 +0100

    Update DataService.cs

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBlogProject.Data;
 
 namespace TheBlogProject.Services
 {
     public class DataService
     {
+        private readonly ApplicationDbContext _context;
+
+        public DataService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task ManageDataASync()
+        {
+            //Create roles
+            await CreateRolesAsync();
+
+            //Create new users
+            await AddUsersAsync();
+        }
+
+        private async Task CreateRolesAsync()
+        {
+
+        }
+
+
+        private async Task AddUsersAsync()
+        {
+
+        }
+
+
+
+
+
     }
 }

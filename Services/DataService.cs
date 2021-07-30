6e37f21fb900787b6d0d5a2c03b6e871b725909a
razvanr91 <razvanroman1991@gmail.com>
2021-07-30 15:45:45 +0100

    Create Data Service

---
+++ Services/DataService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBlogProject.Services
+{
+    public class DataService
+    {
+    }
+}

801a6fb5ec39a79b5367ede5293d9d3f3bce8eaa
razvanr91 <razvanroman1991@gmail.com>
2021-07-31 14:27:08 +0100

    Create Mail Settings Class

---
+++ ViewModels/MailSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBlogProject.ViewModels
+{
+    public class MailSettings
+    {
+        // Configure and use SMTP Server
+        // from Google
+        public string Mail { get; set; }
+
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+    }
+}

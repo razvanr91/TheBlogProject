473f11f60abff131ab61ba33dfac107f6e5efb7f
razvanr91 <razvanroman1991@gmail.com>
2021-08-01 22:57:53 +0100

    Create Image Service Interface

---
+++ Services/IImageService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBlogProject.Services
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile file);
+
+        Task<byte[]> EncodeImageAsync(string fileName);
+
+        string DecodeImage(byte[] imageData, string type);
+
+        string ContentType(IFormFile file);
+
+        int Size();
+    }
+}
---
+++ Services/ImageService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBlogProject.Services
+{
+    public class ImageService : IImageService
+    {
+        public string ContentType(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DecodeImage(byte[] imageData, string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> EncodeImageAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> EncodeImageAsync(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Size()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

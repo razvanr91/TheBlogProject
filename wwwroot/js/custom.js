f88cb42ab46bea35aaa4fae547d45c2e87c96247
razvanr91 <razvanroman1991@gmail.com>
2021-08-04 13:33:06 +0100

    Add new Tag funtionality

--- wwwroot/js/custom.js
+++ wwwroot/js/custom.js
@@ -1,1 +1,18 @@
-﻿
+﻿let index = 0;
+
+function AddTag() {
+    let tagEntry = document.getElementById("TagEntry");
+
+    let newOption = new Option(tagEntry.value, tagEntry.value);
+
+    document.getElementById("TagValues").options[index++] = newOption;
+
+    tagEntry.value = "";
+    return true;
+}
+
+function DeleteTag() {
+    let tagValues = document.getElementById("TagValues");
+    let selectedTagIndex = tagValues.selectedIndex;
+    tagValues.options.DeleteTag(selectedTagIndex);
+}
